ab82bec23f426c328dfbe1231e50e222da2bb99e
PatrickCookSAIT <patrick.cook@edu.sait.ca>
2026-05-27 12:01:29 -0600

    edits to movie bios

--- components/the-room.tsx
+++ components/the-room.tsx
@@ -2,6 +2,7 @@ import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
 import {
     Dimensions,
+    Image,
     ImageBackground,
     StyleSheet,
     Text,
@@ -18,7 +19,7 @@ const TheRoom = () => {
         style={styles.backgroundImage}
       >
         <LinearGradient
-          //4 transparent tranistions to match style of letterboxd app
+          //4 transparent transitions to match style of letterboxd app
           colors={[
             "transparent",
             "transparent",
@@ -30,7 +31,15 @@ const TheRoom = () => {
         />
       </ImageBackground>
       <View style={styles.movieBioContainer}>
-        <Text style={styles.movieTitle}>The Room</Text>
+        <View style={styles.movieBioDetailsContainer}>
+          <Text style={styles.movieTitle}>The Room</Text>
+        </View>
+        <Image
+          source={{
+            uri: "https://a.ltrbxd.com/resized/sm/upload/qq/yi/i3/dk/aUC39cFC2KO8CJ0EV0ijIJRr3PT-0-1000-0-1500-crop.jpg?v=95164ef310",
+          }}
+          style={styles.moviePoster}
+        />
       </View>
     </View>
   );
@@ -54,11 +63,28 @@ const styles = StyleSheet.create({
   },
   movieBioContainer: {
     paddingTop: 15,
+    flexDirection: "row",
+  },
+  movieBioDetailsContainer: {
+    flexDirection: "column",
+    width: "60%",
+    padding: 10,
   },
   movieTitle: {
     fontSize: 24,
     fontFamily: "Arial",
     fontWeight: "700",
     color: "#fff",
+  },
+  moviePoster: {
+    width: "35%",
+    aspectRatio: 2 / 3,
+    marginTop: 10,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#fff",
+  },
+  sectionContainer: {
+    paddingTop: 15,
   },
 });
