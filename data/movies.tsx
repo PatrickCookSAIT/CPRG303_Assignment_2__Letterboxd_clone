01546bb77a225fe01d7691760c8a70af78afbdb0
PatrickCookSAIT <patrick.cook@edu.sait.ca>
2026-06-17 13:31:28 -0600

    all movie details up to date

--- data/movies.tsx
+++ data/movies.tsx
@@ -18,7 +18,8 @@ export const MOVIES = [
     title: "Cool Cat Fights Corona Virus",
     posterImage:
       "https://a.ltrbxd.com/resized/film-poster/1/0/6/8/8/5/9/1068859-cool-cat-fights-coronavirus-0-1000-0-1500-crop.jpg?v=5a93d9f990",
-    stillBackdrop: "",
+    stillBackdrop:
+      "https://a.ltrbxd.com/resized/sm/upload/xv/cw/l4/5f/77IjWG7ofataJjxLYTT8TYP6C44-1200-1200-675-675-crop-000000.jpg?v=b3d6ac89c3",
     releaseYear: "2021",
     director: "Derek Savage",
     runTime: "20 mins",
@@ -31,7 +32,8 @@ export const MOVIES = [
     title: "Fateful Findings",
     posterImage:
       "https://a.ltrbxd.com/resized/film-poster/1/4/1/8/6/6/141866-fateful-findings-0-1000-0-1500-crop.jpg?v=e6a020c8e1",
-    stillBackdrop: "",
+    stillBackdrop:
+      "https://a.ltrbxd.com/resized/sm/upload/2n/20/5x/h3/fateful-findings-1200-1200-675-675-crop-000000.jpg?v=e38abcc289",
     releaseYear: "2013",
     director: "Neil Breen",
     runTime: "100 mins",
@@ -44,7 +46,8 @@ export const MOVIES = [
     title: "Mega Shark vs. Giant Octopus",
     posterImage:
       "https://a.ltrbxd.com/resized/film-poster/4/0/6/9/6/40696-mega-shark-vs-giant-octopus-0-1000-0-1500-crop.jpg?v=7c30243e33",
-    stillBackdrop: "",
+    stillBackdrop:
+      "https://a.ltrbxd.com/resized/sm/upload/ya/ns/u4/lx/MegaShark-1200-1200-675-675-crop-000000.jpg?v=8bd486628c",
     releaseYear: "2009",
     director: "Jack Perez",
     runTime: "90 mins",
@@ -57,7 +60,8 @@ export const MOVIES = [
     title: "2025: The World Enslaved by a Virus",
     posterImage:
       "https://a.ltrbxd.com/resized/film-poster/7/2/7/4/4/9/727449-2025-the-world-enslaved-by-a-virus-0-1000-0-1500-crop.jpg?v=7ba371068c",
-    stillBackdrop: "",
+    stillBackdrop:
+      "https://a.ltrbxd.com/resized/sm/upload/sk/q6/yt/g7/2025-1200-1200-675-675-crop-000000.jpg?v=130a1c6e5c",
     releaseYear: "2021",
     director: "Simon Wesely, Joshua Wesely",
     runTime: "91 mins",
@@ -66,11 +70,12 @@ export const MOVIES = [
       "In 2025, covid has achieved its ultimate goal, making christianity illegal. Its up to a ragtag group of podcasters and youtubers to save the day and make the world free once more.",
   },
   {
-    code: "",
-    title: "",
+    code: "HotFrosty",
+    title: "Hot Frosty",
     posterImage:
-      "https://a.ltrbxd.com/resized/film-poster/1/1/7/3/4/2/3/1173423-hot-frosty-0-1000-0-1500-crop.jpg?v=3c1c42b6f5",
-    stillBackdrop: "",
+      "https://a.ltrbxd.com/resized/alternative-backdrop/1/1/7/3/4/2/3/tmdb/eabjqu3JzL7j172ojXKYevNUaFa-1200-1200-675-675-crop-000000.jpg?v=caf630dbea",
+    stillBackdrop:
+      "https://a.ltrbxd.com/resized/alternative-backdrop/1/1/7/3/4/2/3/tmdb/eabjqu3JzL7j172ojXKYevNUaFa-1200-1200-675-675-crop-000000.jpg?v=caf630dbea",
     releaseYear: "2024",
     director: "Jerry Ciccoritti",
     runTime: "90 mins",
@@ -83,7 +88,8 @@ export const MOVIES = [
     title: "Superfast!",
     posterImage:
       "https://a.ltrbxd.com/resized/film-poster/2/5/2/4/5/1/252451-superfast--0-1000-0-1500-crop.jpg?v=295ae6a988",
-    stillBackdrop: "",
+    stillBackdrop:
+      "https://a.ltrbxd.com/resized/sm/upload/22/vu/h7/na/Superfast-1200-1200-675-675-crop-000000.jpg?v=a58c517f0a",
     releaseYear: "2015",
     director: "Jason Friedberg, Aaron Seltzer",
     runTime: "99 mins",
@@ -96,24 +102,28 @@ export const MOVIES = [
     title: "Hard Ticket to Hawaii",
     posterImage:
       "https://a.ltrbxd.com/resized/film-poster/3/5/6/1/6/35616-hard-ticket-to-hawaii-0-1000-0-1500-crop.jpg?v=2cc58c9f58",
-    stillBackdrop: "",
+    stillBackdrop:
+      "https://a.ltrbxd.com/resized/sm/upload/jn/hz/pm/65/hard%20ticket-1200-1200-675-675-crop-000000.jpg?v=e3ebc42d54",
     releaseYear: "1987",
     director: "Andy Sidaris",
     runTime: "96 mins",
-    synopsisTitle: "",
-    synopsis: "",
+    synopsisTitle: "LIFE ON HAWAII IS EASY, AT LEAST UNTIL IT GETS HARD",
+    synopsis:
+      "Drugs have invaded the beautiful paradise of Hawaii. The only chance to get rid of them is with the powers of teamwork, love and kickboxing",
   },
   {
     code: "SamuraiCop",
     title: "Samurai Cop",
     posterImage:
       "https://a.ltrbxd.com/resized/film-poster/1/4/5/8/1458-samurai-cop-0-1000-0-1500-crop.jpg?v=2003ba3e4b",
-    stillBackdrop: "",
-    releaseYear: "",
-    director: "",
-    runTime: "",
-    synopsisTitle: "",
-    synopsis: "",
+    stillBackdrop:
+      "https://a.ltrbxd.com/resized/sm/upload/16/fi/lv/iy/samurai-cop-1200-1200-675-675-crop-000000.jpg?v=1884d4e30b",
+    releaseYear: "1991",
+    director: "Amir Shervan",
+    runTime: "96 mins",
+    synopsisTitle: "NO MORE MISTER NICE SAMURAI",
+    synopsis:
+      "When the Japanese Yakuza embeds itself deep into the LA criminal underworld, there is only one way to stop them. Sharper than a katana and quicker than a 9mm bullet, Joe Marshall is the samurai cop and the only one who can save LA",
   },
   {
     code: "Plan9FromOuterSpace",
@@ -121,22 +131,24 @@ export const MOVIES = [
     posterImage:
       "https://a.ltrbxd.com/resized/film-poster/4/6/1/5/9/46159-plan-9-from-outer-space-0-2000-0-3000-crop.jpg?v=3d447bc86a",
     stillBackdrop: "",
-    releaseYear: "",
-    director: "",
-    runTime: "",
-    synopsisTitle: "",
-    synopsis: "",
+    releaseYear: "1957",
+    director: "Edward D. Wood Jr.",
+    runTime: "78 mins",
+    synopsisTitle: "IT CAME FROM THE STARS",
+    synopsis:
+      "The first 8 plans failed. But when the 9th planet arrives from outer space, will it be enough to save the day?",
   },
   {
     code: "HotBot",
     title: "Hot Bot",
     posterImage:
       "https://a.ltrbxd.com/resized/film-poster/3/2/0/0/9/0/320090-hot-bot-0-2000-0-3000-crop.jpg?v=e4c0774e94",
+    //main letterboxd page has no backdrop image
     stillBackdrop: "",
-    releaseYear: "",
-    director: "",
-    runTime: "",
-    synopsisTitle: "",
+    releaseYear: "2016",
+    director: "Michael Polis",
+    runTime: "86 mins",
+    synopsisTitle: "THE BOT IS HOT EVEN WHEN YOU ARE NOT",
     synopsis: "",
   },
   {
@@ -144,85 +156,99 @@ export const MOVIES = [
     title: "Troll 2",
     posterImage:
       "https://a.ltrbxd.com/resized/film-poster/3/4/9/4/1/34941-troll-2-0-2000-0-3000-crop.jpg?v=f24546a2b3",
-    stillBackdrop: "",
-    releaseYear: "",
-    director: "",
-    runTime: "",
+    stillBackdrop:
+      "https://a.ltrbxd.com/resized/sm/upload/c4/i0/76/u5/troll2-1200-1200-675-675-crop-000000.jpg?v=89b0fe2703",
+    releaseYear: "1990",
+    director: "Claudio Fragasso",
+    runTime: "95 min",
     synopsisTitle:
       "THEY'RE EATING HER. THEN THEY ARE GOING TO EAT ME. OH MY GOOOOOOOD",
-    synopsis: "",
+    synopsis:
+      "Get trolled once, shame on you. Get trolled twice, can't get trolled again",
   },
   {
     code: "CoolCatSavesTheKids",
     title: "Cool Cat Saves the Kids",
     posterImage:
       "https://a.ltrbxd.com/resized/film-poster/2/5/3/0/0/3/253003-cool-cat-saves-the-kids-0-2000-0-3000-crop.jpg?v=f0c5c800d7",
-    stillBackdrop: "",
-    releaseYear: "",
-    director: "",
-    runTime: "",
-    synopsisTitle: "",
-    synopsis: "",
+    stillBackdrop:
+      "https://a.ltrbxd.com/resized/sm/upload/wa/ao/ga/l1/cool-cat-saves-the-kids-1200-1200-675-675-crop-000000.jpg?v=ca5b4acd9",
+    releaseYear: "2015",
+    director: "Derek Savage",
+    runTime: "75 mins",
+    synopsisTitle: "COOL CAT LOVES YOU",
+    synopsis:
+      "The kids are in danger! Cool Cat, we need your help! Cool Cat leaps heroically into action to save the kids. And always remember, Cool Cat loves you",
   },
   {
     code: "PassThru",
     title: "Pass Thru",
     posterImage:
       "https://a.ltrbxd.com/resized/film-poster/3/0/9/1/8/3/309183-pass-thru-0-2000-0-3000-crop.jpg?v=d6d2d75d80",
-    stillBackdrop: "",
-    releaseYear: "",
-    director: "",
-    runTime: "",
-    synopsisTitle: "",
-    synopsis: "",
+    stillBackdrop:
+      "https://a.ltrbxd.com/resized/sm/upload/rn/s0/04/ap/pass%20thru-1200-1200-675-675-crop-000000.jpg?v=3fd8e3d82c",
+    releaseYear: "2016",
+    director: "Neil Breen",
+    runTime: "90 mins",
+    synopsisTitle: "JUST PASSING THRU",
+    synopsis:
+      "Neil Breen directs, stars, writes, produces, does his own stunts, casts, costumes and stars in a movie about just getting by in a world that doesn't want you to go",
   },
   {
     code: "Roar",
     title: "Roar",
     posterImage:
       "https://a.ltrbxd.com/resized/sm/upload/ek/rx/ho/4o/rEWDgroPVLKHYJcL1bbwEpWyBkh-0-2000-0-3000-crop.jpg?v=8a343ce097",
-    stillBackdrop: "",
-    releaseYear: "",
-    director: "",
-    runTime: "",
+    stillBackdrop:
+      "https://a.ltrbxd.com/resized/sm/upload/ek/rx/ho/4o/rEWDgroPVLKHYJcL1bbwEpWyBkh-0-2000-0-3000-crop.jpg?v=8a343ce097",
+    releaseYear: "1981",
+    director: "Noel Marshall",
+    runTime: "95 mins",
     synopsisTitle: "ANIMALS DON'T SHOUT. THEY ROAR",
-    synopsis: "",
+    synopsis:
+      "A family's idyllic life operating an African animal refugee shatters when the animals go on a rampage. No animals were harmed in the making of this film. All humans were.",
   },
   {
     code: "NoHoldsBarred",
     title: "No Holds Barred",
     posterImage:
       "https://a.ltrbxd.com/resized/film-poster/2/5/6/3/7/25637-no-holds-barred-0-2000-0-3000-crop.jpg?v=e398626a3a",
-    stillBackdrop: "",
-    releaseYear: "",
-    director: "",
-    runTime: "",
-    synopsisTitle: "",
-    synopsis: "",
+    stillBackdrop:
+      "https://a.ltrbxd.com/resized/sm/upload/cp/58/yt/29/1upyFSMYBu2kKeGqPNoE039Wvn3-1200-1200-675-675-crop-000000.jpg?v=25c8240856",
+    releaseYear: "1989",
+    director: "Thomas J. Wright",
+    runTime: "93 mins",
+    synopsisTitle: "WHAT'S THAT SMELL? DOOKIE?",
+    synopsis:
+      "No holds are barred in this action extravaganza. Pure Hogan. Pure Americana. Pure Wrestling at its finest",
   },
   {
     code: "StopOrMyMomWillShoot",
     title: "Stop or My Mom Will Shoot",
     posterImage:
       "https://a.ltrbxd.com/resized/film-poster/4/6/7/5/7/46757-stop-or-my-mom-will-shoot-0-2000-0-3000-crop.jpg?v=e384237708",
-    stillBackdrop: "",
-    releaseYear: "",
-    director: "",
-    runTime: "",
-    synopsisTitle: "",
-    synopsis: "",
+    stillBackdrop:
+      "https://a.ltrbxd.com/resized/sm/upload/z7/0p/gg/9b/stop-1200-1200-675-675-crop-000000.jpg?v=3bb3ee363c",
+    releaseYear: "1992",
+    director: "Roger Spottiswoode",
+    runTime: "87 mins",
+    synopsisTitle: "THE EXPENDABLES IF IT WAS GOOD",
+    synopsis:
+      "When an overbearing mom decides that the criminals need to give her cop son a day off. She is about to show the underworld some motherly love",
   },
   {
     code: "Megalopolis",
     title: "Megalopolis",
     posterImage:
       "https://a.ltrbxd.com/resized/film-poster/5/2/0/3/2/8/520328-megalopolis-0-2000-0-3000-crop.jpg?v=64ab4fa858",
-    stillBackdrop: "",
-    releaseYear: "",
-    director: "",
-    runTime: "",
-    synopsisTitle: "",
-    synopsis: "",
+    stillBackdrop:
+      "https://a.ltrbxd.com/resized/sm/upload/2j/7y/oq/xj/mega-1200-1200-675-675-crop-000000.jpg?v=d8737327e7",
+    releaseYear: "2024",
+    director: "Francis Ford Coppola",
+    runTime: "138 mins",
+    synopsisTitle: "IF YOU BUILD IT THEY WILL COME",
+    synopsis:
+      "Ambition. Drive. Purpose. Bureaucracy. Do not let the world tell you what you can build. Build it. Ask for apology and permits after.",
   },
   {
     code: "MadameWeb",
@@ -230,11 +256,12 @@ export const MOVIES = [
     posterImage:
       "https://a.ltrbxd.com/resized/film-poster/5/6/0/6/9/7/560697-madame-web-0-2000-0-3000-crop.jpg?v=305a4aead1",
     stillBackdrop: "",
-    releaseYear: "",
-    director: "",
-    runTime: "",
-    synopsisTitle: "",
-    synopsis: "",
+    releaseYear: "2024",
+    director: "SJ Clarkson",
+    runTime: "117 mins",
+    synopsisTitle: "THE WEB OF FATE BINDS US ALL",
+    synopsis:
+      "That man is Ezekiel Simms. He was in the Amazon with my mom when she was researching spiders right before she died",
   },
 ];
 
