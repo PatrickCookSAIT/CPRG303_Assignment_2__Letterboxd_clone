c0ccd74991af01240d390d16f14206f3183f7031
PatrickCookSAIT <patrick.cook@edu.sait.ca>
2026-06-21 11:36:52 -0600

    Update userpagedata.tsx

---
+++ data/userpagedata.tsx
@@ -0,0 +1,37 @@
+export const USERS = [
+  {
+    code: "PatrickCook",
+    name: "PatrickCook",
+    userPFP: "",
+    recentActivity: [
+      {
+        movieCode: "TheRoom",
+        movieRating: 5,
+      },
+      {
+        movieCode: "FatefulFindings",
+        movieRating: 4,
+      },
+      {
+        movieCode: "2025TheWorldEnslavedByAVirus",
+        movieRating: 1,
+      },
+      {
+        movieCode: "NoHoldsBarred",
+        movieRating: 2,
+      },
+      {
+        movieCode: "Roar",
+        movieRating: 5,
+      },
+      {
+        movieCode: "HotFrosty",
+        movieRating: 2,
+      },
+      {
+        movieCode: "PassThru",
+        movieRating: 5,
+      },
+    ],
+  },
+];
